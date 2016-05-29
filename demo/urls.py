a4e252667edd9e9340f11f207dba8bbc1788f93e
Axel Haustant <noirbizarre@gmail.com>
2016-05-29 02:33:58 +0200

    First drone strike

---
+++ demo/urls.py
@@ -0,0 +1,31 @@
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.views.generic.base import RedirectView
+
+from wagtail.wagtailcore import urls as wagtail_urls
+from wagtail.wagtailadmin import urls as wagtailadmin_urls
+from wagtail.wagtaildocs import urls as wagtaildocs_urls
+from wagtail.wagtailsearch.urls import frontend as wagtailsearch_frontend_urls
+from wagtail.wagtailsearch.signal_handlers import register_signal_handlers as wagtailsearch_register_signal_handlers
+import os
+wagtailsearch_register_signal_handlers()
+
+
+urlpatterns = patterns('',
+    # url(r'^blog/', include('blog.urls', namespace="blog")),
+    url(r'^django-admin/', include(admin.site.urls)),
+    url(r'^admin/', include(wagtailadmin_urls)),
+    # url(r'^comments/', include('django_comments_xtd.urls')),
+    url(r'', include(wagtail_urls)),
+    url(r'^docs/', include(wagtaildocs_urls)),
+)
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL + 'images/', document_root=os.path.join(settings.MEDIA_ROOT, 'images'))
+    urlpatterns += patterns('',
+        (r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'demo/images/favicon.ico'))
+    )
---
+++ tasks.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+from invoke import run, task
+
+from os.path import join, abspath, dirname
+
+ROOT = abspath(join(dirname(__file__)))
+
+
+def lrun(cmd, *args, **kwargs):
+    '''Run a command ensuring cwd is project root'''
+    return run('cd {0} && {1}'.format(ROOT, cmd), *args, **kwargs)
+
+
+@task
+def clean(docs=False, bytecode=False, extra=''):
+    '''Cleanup all build artifacts'''
+    patterns = ['build', 'dist', 'cover', 'docs/_build', '**/*.pyc', '*.egg-info', '.tox']
+    for pattern in patterns:
+        print('Removing {0}'.format(pattern))
+        lrun('rm -rf {0}'.format(pattern))
+
+
+@task
+def demo():
+    '''Run the demo'''
+    lrun('python examples/todo.py')
+
+
+@task
+def test():
+    '''Run tests suite'''
+    lrun('nosetests --force-color', pty=True)
+
+
+@task
+def cover():
+    '''Run tests suite with coverage'''
+    lrun('nosetests --force-color --with-coverage --cover-html', pty=True)
+
+
+@task
+def tox():
+    '''Run tests against Python versions'''
+    run('tox', pty=True)
+
+
+@task
+def qa():
+    '''Run a quality report'''
+    lrun('flake8 wapps')
+
+
+@task
+def doc():
+    '''Build the documentation'''
+    lrun('cd doc && make html', pty=True)
+
+
+@task
+def update():
+    '''Update dependancies'''
+    lrun('pip install -r requirements/develop.pip')
+    lrun('npm install')
+
+
+@task
+def assets():
+    '''Fetch web assets'''
+    lrun('npm run build')
+
+
+@task
+def dist():
+    '''Package for distribution'''
+    lrun('python setup.py sdist bdist_wheel', pty=True)
+
+
+@task(tox, doc, qa, assets, dist, default=True)
+def all():
+    '''Run tests, reports and packaging'''
+    pass
