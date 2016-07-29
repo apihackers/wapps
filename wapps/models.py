d2a298aeb73a519113df83d74d965137263a2111
Axel Haustant <noirbizarre@gmail.com>
2016-07-29 13:41:34 +0200

    Added identity logo

--- wapps/models.py
+++ wapps/models.py
@@ -9,6 +9,7 @@ from taggit.models import TaggedItemBase
 
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 from .mixins import SocialFields
 from .utils import mark_safe_lazy
@@ -29,11 +30,21 @@ class IdentitySettings(SocialFields, ClusterableModel, BaseSetting):
     description = models.TextField(_('Description'), max_length=255, null=True, blank=True,
                                   help_text=mark_safe_lazy(_('A short entity description')))
 
+    logo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        verbose_name=_('Logo'),
+    )
+
     tags = ClusterTaggableManager(_('Tags'), through=IdentityTag, blank=True)
 
     panels = [
         FieldPanel('name'),
         FieldPanel('description'),
         FieldPanel('tags'),
+        ImageChooserPanel('logo'),
         MultiFieldPanel(SocialFields.panels, heading=_('Social networks'), classname='collapsible'),
     ]
