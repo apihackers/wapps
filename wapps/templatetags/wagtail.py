afa3d1fa3a9522f646dc964b91abbfb83212cd0c
Axel Haustant <noirbizarre@gmail.com>
2016-08-21 19:43:08 +0200

    Removed legacy render_stream and render_struct

--- wapps/templatetags/wagtail.py
+++ wapps/templatetags/wagtail.py
@@ -1,7 +1,6 @@
 import jinja2
 
 from django.conf import settings
-from django.template.loader import get_template
 from django_jinja import library
 from jinja2.ext import Extension
 from wagtail.wagtailcore.models import Page
@@ -19,36 +18,6 @@ def is_site_root(context, page):
         return False
     site = context['request'].site
     return site.root_page.pk == page.pk
-
-
-@library.global_function
-@jinja2.contextfunction
-def render_stream(context, stream_child):
-    # Use the django_jinja to get the template content based on its name
-    template = get_template(stream_child.block.meta.template)
-    # Create a new context based on the current one as we can't edit it directly
-    new_context = context.get_all()
-    # Add the value on the context (value is the keyword chosen by wagtail for the blocks context)
-    new_context.update(stream_child.block.get_context(stream_child.value))
-    # Render the template with the context
-    html = template.render(context=new_context)
-    # Return the rendered template as safe html
-    return jinja2.Markup(html)
-
-
-@library.global_function
-@jinja2.contextfunction
-def render_struct(context, struct_value):
-    # Use the django_jinja to get the template content based on its name
-    template = get_template(struct_value.block.meta.template)
-    # Create a new context based on the current one as we can't edit it directly
-    new_context = context.get_all()
-    # Add the value on the context (value is the keyword chosen by wagtail for the blocks context)
-    new_context['value'] = struct_value
-    # Render the template with the context
-    html = template.render(context=new_context)
-    # Return the rendered template as safe html
-    return jinja2.Markup(html)
 
 
 @library.extension
