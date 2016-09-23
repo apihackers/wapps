915005a412de6b3f1dcc9f14b8e2e379f511c168
Axel Haustant <noirbizarre@gmail.com>
2016-09-23 10:25:18 +0200

    Added Debug Toolbar fix

---
+++ wapps/debug_toolbar.py
@@ -0,0 +1,10 @@
+# debug.py
+from debug_toolbar.panels.templates import TemplatesPanel as BaseTemplatesPanel
+
+
+class TemplatesPanel(BaseTemplatesPanel):
+    def generate_stats(self, *args):
+        template = self.templates[0]['template']
+        if not hasattr(template, 'engine') and hasattr(template, 'backend'):
+            template.engine = template.backend
+        return super().generate_stats(*args)
