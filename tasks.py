6e87075550e3ecd295ec2000041f76c17dcf5a07
Axel Haustant <noirbizarre@gmail.com>
2016-10-18 17:23:24 +0200

    Upgraded to invoke 0.13

--- tasks.py
+++ tasks.py
@@ -1,83 +1,83 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from invoke import run, task
+from invoke import run as irun, task
 
 from os.path import join, abspath, dirname
 
 ROOT = abspath(join(dirname(__file__)))
 
 
-def lrun(cmd, *args, **kwargs):
+def run(cmd, *args, **kwargs):
     '''Run a command ensuring cwd is project root'''
-    return run('cd {0} && {1}'.format(ROOT, cmd), *args, **kwargs)
+    return irun('cd {0} && {1}'.format(ROOT, cmd), *args, **kwargs)
 
 
 @task
-def clean(docs=False, bytecode=False, extra=''):
+def clean(ctx, docs=False, bytecode=False, extra=''):
     '''Cleanup all build artifacts'''
     patterns = ['build', 'dist', 'cover', 'docs/_build', '**/*.pyc', '*.egg-info', '.tox']
     for pattern in patterns:
         print('Removing {0}'.format(pattern))
-        lrun('rm -rf {0}'.format(pattern))
+        run('rm -rf {0}'.format(pattern))
 
 
 @task
-def demo():
+def demo(ctx):
     '''Run the demo'''
-    lrun('python examples/todo.py')
+    run('python examples/todo.py')
 
 
 @task
-def test():
+def test(ctx):
     '''Run tests suite'''
-    lrun('nosetests --force-color', pty=True)
+    run('nosetests --force-color', pty=True)
 
 
 @task
-def cover():
+def cover(ctx):
     '''Run tests suite with coverage'''
-    lrun('nosetests --force-color --with-coverage --cover-html', pty=True)
+    run('nosetests --force-color --with-coverage --cover-html', pty=True)
 
 
 @task
-def tox():
+def tox(ctx):
     '''Run tests against Python versions'''
     run('tox', pty=True)
 
 
 @task
-def qa():
+def qa(ctx):
     '''Run a quality report'''
-    lrun('flake8 wapps')
+    run('flake8 wapps')
 
 
 @task
-def doc():
+def doc(ctx):
     '''Build the documentation'''
-    lrun('cd doc && make html', pty=True)
+    run('cd doc && make html', pty=True)
 
 
 @task
-def update():
+def update(ctx):
     '''Update dependancies'''
-    lrun('pip install -r requirements/develop.pip')
-    lrun('npm install')
+    run('pip install -r requirements/develop.pip')
+    run('npm install')
 
 
 @task
-def assets():
+def assets(ctx):
     '''Fetch web assets'''
-    lrun('npm run build')
+    run('npm run build')
 
 
 @task
-def dist():
+def dist(ctx):
     '''Package for distribution'''
-    lrun('python setup.py sdist bdist_wheel', pty=True)
+    run('python setup.py sdist bdist_wheel', pty=True)
 
 
 @task(tox, doc, qa, assets, dist, default=True)
-def all():
+def all(ctx):
     '''Run tests, reports and packaging'''
     pass
