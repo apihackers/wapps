c4e46baf7b425bda2c72d50b36bccce25e69a7bb
Axel Haustant <noirbizarre@gmail.com>
2017-01-27 04:37:40 +0100

    Upgrade to Wagtail 1.8

--- wapps/models/image.py
+++ wapps/models/image.py
@@ -24,7 +24,7 @@ class WappsRendition(AbstractRendition):
 
     class Meta:
         unique_together = (
-            ('image', 'filter', 'focal_point_key'),
+            ('image', 'filter_spec', 'focal_point_key'),
         )
 
 
