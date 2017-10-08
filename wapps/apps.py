548030c23ae2937a9023bde6ba9596d8ee2a5fe8
Axel Haustant <noirbizarre@gmail.com>
2017-10-08 20:59:35 +0200

    Use Django AppConf

--- wapps/apps.py
+++ wapps/apps.py
@@ -1,7 +1,23 @@
 from django.apps import AppConfig
+from appconf import AppConf
 
 
 class WappsConfig(AppConfig):
     name = 'wapps'
     label = 'wapps'
     verbose_name = 'Wagtail Apps'
+
+
+class WappsSettings(AppConf):
+    '''Wapps default settings'''
+    FEATURES = (
+        'word_count',
+    )
+
+    class Meta:
+        prefix = 'wapps'
+
+
+class WagtailSettings(AppConf):
+    '''Wagtail default settings'''
+    WAGTAILIMAGES_IMAGE_MODEL = 'wapps.WappsImage'
