17b64654f8e2554f9736fc252096779c0169a8fd
Axel Haustant <noirbizarre@gmail.com>
2017-10-28 15:43:32 +0200

    Added some CircleCI tools

---
+++ .circleci/tools.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Create a self signed certificate
+base_branch() {
+    # Get the base branch against which a PR has been made
+    # Extracted from https://discuss.circleci.com/t/how-to-get-the-pull-request-upstream-branch/5496/3
+    # Until https://discuss.circleci.com/t/expose-the-title-and-upstream-branch-of-a-pull-request-build-as-an-env/5475/1
+    # is implemented
+
+    # Set a default in case we run into rate limit restrictions
+    BASE_BRANCH="master"
+    if [[ $CIRCLE_PR_NUMBER ]]; then
+      BASE_BRANCH=$(curl -fsSL https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/$CIRCLE_PR_NUMBER | jq -r '.base.ref')
+    elif [[ $CIRCLE_TAG ]]; then
+      BASE_BRANCH='master'
+    elif [[ $CIRCLE_BRANCH ]]; then
+      BASE_BRANCH=$CIRCLE_BRANCH
+    fi
+    echo $BASE_BRANCH
+}
